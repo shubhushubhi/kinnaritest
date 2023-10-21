bcf5cbffbf9ef6ba7db9fe5f51c5cd7d1c4c5727
shubhushubhi <kinnari1590@gmail.com>
2023-10-21 13:59:02 +0100

    kinnaritest

---
+++ src/main/java/org/example/test01.java
@@ -0,0 +1,8 @@
+package org.example;
+// Write a program that prints your BIO Data.
+public class test01 {
+    public static void main(String[]args){
+        System.out.println(" Name-Kinnari Adhyaru \n DOB 15/07/85 \n Live in london ");
+
+    }
+}
